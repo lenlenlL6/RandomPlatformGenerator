4b2a4c18e17e092d7220c61a93cbf79046a0c467
Karyl <62021442+lenlenlL6@users.noreply.github.com>
2024-07-14 18:31:35 +0700

    update

---
+++ main.lua
@@ -0,0 +1,20 @@
+world = require("world/world")
+chunk = require("world/chunk")
+
+GAME_WIDTH, GAME_HEIGHT = 1920, 1080
+
+function love.load()
+    love.window.setMode(GAME_WIDTH, GAME_HEIGHT, {fullscreen = true})
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+
+end
+
+function love.quit()
+
+end
---
+++ world/chunk.lua
@@ -0,0 +1,18 @@
+local chunk = {}
+
+function chunk:new(chunkData)
+    local obj = {}
+    setmetatable(obj, {__index = self})
+    obj.x = chunkData.x
+    obj.data = chunkData.y
+    return obj
+end
+
+function chunk:render()
+    for y, v in pairs(self.data) do
+        for x, value in pairs(v) do
+            if value > 0 then
+                love.graphics.rectangle("fill", self.x * 512 + (x - 1) * 32, (y - 1) * 32 + GAME_HEIGHT / 2)
+        end
+    end
+end
